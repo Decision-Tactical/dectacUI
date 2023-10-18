792d4353fad82c57cd492f1b290a288d26b9bad6
singhmohan005 <singhmohan005@gmail.com>
2023-10-18 06:51:06 +0530

    UI Changes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+var app = express();
+app.use(cors())
+app.use(express.static('dist/gdms'));
+app.get('/', function (req, res,next) {
+    res.redirect('/');
+});
+ 
+app.get("/:universalURL", (req, res) => { 
+     res.redirect('/'); 
+}); 
+app.listen(8080)
